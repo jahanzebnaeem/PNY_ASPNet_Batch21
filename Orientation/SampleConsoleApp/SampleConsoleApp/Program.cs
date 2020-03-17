6e53a009b3ef8f22a181f264aac844bf50401cc0
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-03-17 21:10:44 +0500

    Orientation
    Batch 21 orientation class.

---
+++ Orientation/SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome to ASP.Net Batch 21, orientation " + name);
+        }
+    }
+}
